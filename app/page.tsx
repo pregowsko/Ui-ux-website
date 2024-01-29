33c3c24600d546e1c52d0fb93738d6603edf4a54
Aleksandra Stanek <ola.pregowska975@gmail.com>
2024-01-29 19:22:07 +0100

    added Navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import { Navbar } from "./components/Navbar";
 
 export default function Home() {
   return (
-    <h1>hello from index page </h1>
+    <>
+      <Navbar />
+    </>
   );
 }
