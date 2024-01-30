4fec50f34260b871b608451bb9a94ba3829448cb
Aleksandra Stanek <ola.pregowska975@gmail.com>
2024-01-30 14:12:54 +0100

    added Faq component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { Features } from "./components/Features";
+import { Faq } from "./components/Faq";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <div className="px-[20px] lg:container lg:px-20 mx-auto">
         <Features />
+        <Faq />
       </div>
     </>
   );
